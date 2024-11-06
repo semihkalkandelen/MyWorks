8443d4b3d40840732d778fe69b931011fa5969d9
semihkalkandelen <semihkdelen@gmail.com>
2024-11-06 14:35:05 +0300

    easy python

---
+++ Collatz conjecture.py
@@ -0,0 +1,15 @@
+"""
+Collatz conjecture
+in the 3x+1 problem, no matter what number you start with, you will always eventually reach 1.
+"""
+
+num = int(input("enter the number and I will show you how it is going to reach 1: "))
+while True:
+    if num == 1:
+        break
+    elif num % 2 == 0:
+        print(f"{num} is even\n{num}/2 = ", int(num/2))
+        num = int(num/2)
+    else:
+        print(f"{num} is odd\n{num}*3+1 = ", int(num*3+1))
+        num = int(num*3+1)
---
+++ perfect_number.py
@@ -0,0 +1,17 @@
+"""
+Perfect Number
+a perfect number is a positive integer that is equal to the sum of its positive proper divisors, 
+that is, divisors excluding the number itself.
+"""
+
+num = int(input("input the number and I will say if it is perfect number or not: "))
+count = 1
+for i in range(2,num):
+    if num % i == 0:
+        count = count + i
+    else:
+        pass
+if count == num :
+    print("number is perfect number")
+else:
+    print("number is not perfect number")
