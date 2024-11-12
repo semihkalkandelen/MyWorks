f8dd636770265ee86aefca17aa9998f93bae0eda
semihkalkandelen <semihkdelen@gmail.com>
2024-11-12 12:54:15 +0300

    normal python

---
+++ find_second_maximum_number_in_a_list.py
@@ -0,0 +1,28 @@
+"""
+Given the participants' score sheet for your University Sports Day, 
+you are required to find the runner-up score. You are given 'n' scores. 
+Store them in a list and find the score of the runner-up.
+"""
+
+mylist = []
+count = 1
+print("Enter the scores. for end the sequence press 'x' ")
+while True:
+    scores = input(f"Score {count} is: ")
+    if scores.isnumeric() == True:
+        mylist.append(scores)
+        count += 1
+    elif scores == "x":
+        if len(mylist) == 1:
+            value = mylist.pop()
+            print(f"you enter only one number and it is: {value}")
+            break
+        elif len(mylist) == 0:
+            print("you did not enter any number.")
+            continue
+        else:
+            mylist=sorted(mylist)
+            print("second highest value you entered is: ", mylist[-2])
+        break
+    else:
+        print("please enter a valid value (x or a score)")
